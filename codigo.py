8ce76761ce2d088b2f66f292cf8e3aaa7fbeab66
pabloandresarg <3004456600101@ingenieria.usac.edu.gt>
2022-01-02 23:49:11 -0600

    fix issue

--- codigo.py
+++ codigo.py
@@ -5,10 +5,38 @@ from sklearn.cluster import KMeans
 
 X = np.array([[8,2],[9,7],[2,12], [9,1], [10,7] , [3,14] ,[8,1] ,[1,13]])
 
-Kmeans = KMeans (n_clusters=3)
-Kmeans.fit(X)
+#                                     primera  iteracion
+# CENTROIDE = np.array([[10,7],])
+# kmeans = KMeans(n_clusters=1, init=CENTROIDE, n_init=1)
+# kmeans.fit(X)
+# plt.show()
 
+#                                     segunta iteracion 
+# CENTROIDE = np.array([[10,7],[6.25,7.125]])
+# kmeans = KMeans(n_clusters=2, init=CENTROIDE , n_init=1)
+# kmeans.fit(X)
+# plt.show()
+
+#                                       tercerda iteracion
+# CENTROIDE = np.array([[10,7],[8.8,3.6],[2,13]])
+# kmeans = KMeans(n_clusters=3, init=CENTROIDE , n_init=1)
+# kmeans.fit(X)
+# plt.show()
+
+
+
+
+Kmeans = KMeans(n_clusters=3)
+Kmeans.fit(X)
 print(Kmeans.cluster_centers_)
 plt.scatter(X[:,0],X[:,1] , c =Kmeans.labels_ , cmap='rainbow')
-# muestra la grafica
+# plt.scatter(kmeans.cluster_centers_[:, 0],kmeans.cluster_centers_[:, 1],color="blue")
+
+
+# predicciones:
+# predicts = kmeans.predict(X)
+# print("***********************")
+# print("predicciones: ")
+# print(predicts)
+# print("***********************")
 plt.show()
