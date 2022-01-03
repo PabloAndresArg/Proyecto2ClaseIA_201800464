db2f47783c0cf06418fd35a2494de8af09614c05
pabloandresarg <3004456600101@ingenieria.usac.edu.gt>
2022-01-02 23:49:54 -0600

    ok

--- codigo.py
+++ codigo.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
 
-X = np.array([[8,2],[9,7],[2,12], [9,1], [10,7] , [3,14] ,[8,1] ,[1,13]])
+
 
 #                                     primera  iteracion
 # CENTROIDE = np.array([[10,7],])
@@ -25,7 +25,7 @@ X = np.array([[8,2],[9,7],[2,12], [9,1], [10,7] , [3,14] ,[8,1] ,[1,13]])
 
 
 
-
+X = np.array([[8,2],[9,7],[2,12], [9,1], [10,7] , [3,14] ,[8,1] ,[1,13]])
 Kmeans = KMeans(n_clusters=3)
 Kmeans.fit(X)
 print(Kmeans.cluster_centers_)
